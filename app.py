b750a27dcda47103e5f847a9c90e538ec10a3f66
Doug Edgar <dedgar@redhat.com>
2019-07-02 14:29:09 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ from bottle import route, run
 def index():
     return "SELECT kanj, von, vkun, transl, roma FROM info WHERE"
 
+@route('/')
+def index():
+    return "<title>Sign in or Register | eBay</title>"
+
+
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
